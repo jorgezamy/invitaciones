ee0004e3d25841473871b415e406143fcc0278d3
Jorge Zamora <jorge.zamora60@hotmail.com>
2023-02-07 18:54:14 -0600

    codigo limpio

--- content/js/animation.js
+++ content/js/animation.js
@@ -1,8 +1,9 @@
 function abrir() {
-  var boton = document.getElementById("btnAbrir");
-  var invitacion = document.getElementById("miDiv");
+  var invitacion = document.getElementById("invitacion");
 
   if (invitacion.style.display === "none") {
+    invitacion.classList.add("mostrar");
+
     setTimeout(function () {
       invitacion.style.display = "block";
     }, 500);
@@ -10,10 +11,13 @@ function abrir() {
     invitacion.style.display = "none";
   }
 
+  var boton = document.getElementById("intro");
+
   if (boton.style.display === "none") {
     boton.style.display = "block";
   } else {
     boton.classList.add("ocultar");
+
     setTimeout(function () {
       boton.style.display = "none";
     }, 500);
